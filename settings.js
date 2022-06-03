16d9cc454a89c5f3f034cdcb98ceb8bbbdeac104
Jaka09b <106713069+Jaka09b@users.noreply.github.com>
2022-06-03 20:33:24 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoread = false // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.botname = "NitsuBotz-Mdོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ JakaOfcོ ×፝֟͜×" //nama kalian
+global.myweb ="https://instagram.com/itzme_nitsu" //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/UC7jyjV6kcG6qTV_VcJtJzzA" //bebas asal jan hapus
+global.github = "https/github.com/Jaka09b/" //bebas
+global.email = "jkasmprn@gmail.com" //bebas
+global.region = "Japanese" //bebas
+global.ownernomer = "6285785694474" // nomor wa kalian
+global.ownernomerr = "+6285785694474" //nmr wa kalian
+global.thumbnail = "./image/lolh.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/donasip.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","918156874290"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["6285785694474","6285785694474","60107155057"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© Nitsu-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh JakaOfc' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
